b0b79e94db057b78f7e5b7aedcbd11679f79bd6e
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2021-04-28 13:25:24 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.a1aa20e52b1f67a0430fb36bf05333c5.js"
 );
 
 self.addEventListener('message', (event) => {
